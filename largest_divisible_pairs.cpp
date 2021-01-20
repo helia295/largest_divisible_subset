18d63236328c6844f17844414f1d1aed02dba5fc
Helia Dinh <ldinh@fandm.edu>
2021-01-20 13:35:38 -0500

    Part 2 - HW4 updated with DYNAMIC PROGRAM & TIME COMPLEXITY.

--- largest_divisible_pairs.cpp
+++ largest_divisible_pairs.cpp
@@ -1,14 +1,22 @@
 
-/* This is the RECURSIVE version that I wrote to solve this 
- * problem.
+/* Part 2 - HW4: To answer TIME COMPLEXITY question:
  * 
- * There is a Non-Recursive version of this assignment in
- * another file named largest_divisible_NonRecursive.cpp. */
+ * The time complexity for my Dynamic Programming version of the code is
+ * O(n.logn + n^2 + n). 
+ * The time complexity for my Recursive version of the code is
+ * O(n^n).
+ * 
+ * I think O(n^n) is always bigger than O(n.logn + n^2 + n) (i even plugged
+ * in some random numbers and calculated them to be sure). 
+ * So the time complexity of the dynamic programming code is smaller than
+ * that of recursive code version.
+ *  */
 
 
 #include <iostream>
 #include <vector>
 #include <algorithm>    // sort
+#include <stdexcept>
 
 
 using namespace std;
@@ -30,7 +38,141 @@ string vec_to_string(vector<int> v) {
 }
 
 
-/* This function will help find the LDP subset using recursion method.
+void checkConditions(vector<int> input) {
+	
+	//to check if there is any NEGATIVE argument in the input, throw exception
+	for (unsigned int i = 0; i < input.size(); i++) {
+		
+		if (input.at(i) < 0) {
+			
+			throw invalid_argument("Negative arguments not allowed: " + to_string(input.at(i)));
+		}
+	}
+	
+	//to check if there is any DUPLICATE argument in the input, throw exception
+	for (unsigned int i = 0; i < input.size(); i++) {
+		for (unsigned int j = i + 1; j < input.size(); j++) {
+			if (input.at(i) == input.at(j)) {
+				throw invalid_argument("Duplicate arguments not allowed: " + to_string(input.at(i)));
+			}
+		}
+	}
+}
+
+
+//DYNAMIC PROGRAMMING APPROACH
+/* Time complexity: O(n.logn) + O(n^2) + O(n)
+ * 
+ * The idea is to find the element in the vector with which we can create the 
+ * largest LDP subset (largest in size) and backtrack from that element to 
+ * find THE subset. */
+ 
+vector<int> largest_divisible_pairs(vector<int> input) {
+	
+	int sz = input.size();
+	
+	//if input vector size is 0, return empty vector
+	if (input.size() == 0) { 	
+		return vector<int>();
+	}
+	
+	/* Time complexity: O(n.logn) --at best.
+	 * We sort the vector in increasing order so that we only have to check 
+	 * if the current element is divisible by the previous one (which is supposed
+	 * to be smaller than current). */
+	sort( input.begin() , input.end() ); 		
+	//cout << "sorted vector: " << vec_to_string(input) << endl;
+	
+	int maxSize = 1; 	//to store the max possible size of the result vector
+	
+	/* This is where the dynamic programming happens.
+	 * This vector will store the largest possible size of a largest divisible
+	 * subset that includes the current element as the last element of subset */
+	vector<int> currMaxSize(sz, 1);
+	
+	// Time complexity: O(n^2) 
+	for (int i = 1; i < sz; i++) { 		//loop from element 1 to sz
+		for (int j = 0; j < i; j++) { 	//loop from element 0 to sz-1
+			
+			/* if element at i is divisible by element at j and this can 
+			 * increase the size of the possible largest divisible subset
+			 * (since size of subset including element i < size of subset
+			 * including both j and i) */
+			if (input.at(i) % input.at(j)==0 && currMaxSize.at(j) + 1 > currMaxSize.at(i)) {
+				
+				currMaxSize.at(i) = currMaxSize.at(j) + 1;
+				
+				if (maxSize < currMaxSize.at(i)) {
+					maxSize = currMaxSize.at(i);
+				}
+			}
+		}
+	}
+	
+	//cout << "size vector: " << vec_to_string(currMaxSize) << endl;
+	
+	//create a result vector and push values to it
+	vector<int> result;
+	int curr = -1;
+	
+	// Time complexity: O(n)
+	for (int k = sz - 1; k >= 0; k--) { 	//go backward in the sorted input vector
+		
+		/* if the max possible size of a LD subset created with the current
+		 * element k == the max possible size of a LD subset of the input 
+		 * AND (current element is divisible by element at k OR curr == -1) 
+		 * 
+		 * Note: since the 2nd condition checks if the 2 number are divisible
+		 * by each other, the code cannot push other number with the same currMaxSize
+		 * value (for exp: it cannot push 24 instead of 28 in the original example 
+		 * because 56 is not divisible by 24) */
+		if (currMaxSize.at(k) == maxSize && (curr % input.at(k) == 0 || curr == -1)) {
+			
+			//push the current element at k to result
+			result.push_back(input.at(k));
+			
+			//decrement maxSize because we just added 1 element to result so now
+			//we need to find the element with the next most currMaxSize value
+			maxSize--;
+			
+			//assign the curr value to input element at k to continue our search
+			curr = input.at(k);
+		}
+	}
+	
+	return result;
+}
+
+
+int main() {
+	
+	vector<int> digitsVector{2, 3, 8, 14, 22, 24, 28, 44, 56, 12, 1};
+	//vector<int> digitsVector{22, 1, 44, 52, 6, 39, 14, 28, 88};
+	try {
+		checkConditions(digitsVector);
+		
+		vector<int> LDPvec = largest_divisible_pairs(digitsVector);
+	
+		cout << "Input: " << vec_to_string(digitsVector) << endl;
+		cout << "Answer: " << vec_to_string(LDPvec) << endl;
+		
+	} catch (invalid_argument &excpt) {
+		
+		cout << excpt.what() << endl;
+	}
+}
+
+
+
+
+
+
+
+
+
+
+/* RECURSIVE APPROACH
+ * This function will help find the LDP subset using recursion method.
  * 
  * Params:
  * @ vector<int> input
@@ -43,19 +185,8 @@ string vec_to_string(vector<int> v) {
  * 							Value -1 means Not part of any possible LDP subset.
  * @ int startIdx: the index of the current element
  * @ int prevNum: store the previous number that was just added to currSubset 
- * 
- * EXPLANATION with example [1, 2, 5, 6]: start at 1, currSubset.size is not > result.size, 
- * check 0 > -1 && 1 % 1==0 --> true, make sizeSubset = [0,-1,-1,-1], currSubset = [1], call
- * ^^^^ so sizeSubset.at(0) = 0 because size of LDP subset before it is 0, there's no element 
- * ^^^^ before it, and currSubset was added by the current number that we are looking at: 1 
- * recursion on the next which is 2, currSubset.size is > result.size --> make result = [1], 
- * check 1 > -1 && 2 % 1==0 --> true, make sizeSubset = [0,1,-1,-1], currSubset = [1,2], call
- * recursion on the next which is 5, currSubset.size is > result.size --> make result = [1,2],
- * check 2 > -1 && 5 % 2==0 --> false, go to check with the next 6 (now, prev is still = 2), 
- * currSubset.size is > result.size --> make result = [1,2], 
- * check 2 > -1 && 6 % 2==0 --> true, make sizeSubset = [0,1,-1,2], currSubset = [1,2,6], call
- * recursion on the next which is Nothing, currSubset.size >result.size-->make result =[1,2,6],
- * now going back to pop currSubset 3 times. */
+ * */
+ /*
 void  FindLDPsubset(vector<int> input, vector<int>& result, vector<int> currSubset, vector<int>& sizeSubset, int startIdx, int prevNum) {
 	
 	//if currSubset is bigger than the old result --> make result = currSubset
@@ -66,9 +197,9 @@ void  FindLDPsubset(vector<int> input, vector<int>& result, vector<int> currSubs
 	
 	for (unsigned int i = startIdx; i < input.size(); i++) {
 		
-		//check if the current number is divisible by prevNum (just been added to currSubset
+		//check if the current number is divisible by prevNum (just been added to currSubset)
 		//also if the size of currSubset > the size of the largest LDP subset that the current 
-		//number can be a part of with the elements before it in the sorted vector.
+		//number can be a part of with some elements before it in the sorted vector.
 		if (static_cast<int>(currSubset.size()) > sizeSubset.at(i) && input.at(i) % prevNum == 0) {
 			
 			//update that current number's sizeSubset value into the size of currSubset
@@ -76,6 +207,7 @@ void  FindLDPsubset(vector<int> input, vector<int>& result, vector<int> currSubs
 			
 			//add the current number to currSubset because it could possibly form a LDP subset
 			currSubset.push_back(input.at(i));
+			
 			
 			//make a recursive call on the next number after the current one
 			FindLDPsubset(input, result, currSubset, sizeSubset, i+1, input.at(i));
@@ -89,33 +221,8 @@ void  FindLDPsubset(vector<int> input, vector<int>& result, vector<int> currSubs
 }
 
 
-void checkConditions(vector<int> input) {
-	
-	//to check if there is any NEGATIVE argument, throw exception
-	for (unsigned int i = 0; i < input.size(); i++) {
-		
-		if (input.at(i) < 0) {
-			
-			throw invalid_argument("Negative arguments not allowed!!!");
-		}
-	}
-	
-	//to check if there is any DUPLICATE argument, throw exception
-	for (unsigned int i = 0; i < input.size(); i++) {
-		for (unsigned int j = i + 1; j < input.size(); j++) {
-			if (input.at(i) == input.at(j)) {
-				throw invalid_argument("Duplicate arguments not allowed: " + to_string(input.at(i)));
-			}
-		}
-	}
-}
-
-
 vector<int> largest_divisible_pairs(vector<int> digitsVector) {
 	
-	/* We sort the vector in increasing order so that we only have to check 
-	 * if the curr element is divisible by the previous one instead of both 
-	 * cases. */
 	sort( digitsVector.begin() , digitsVector.end() );
 	checkConditions(digitsVector); 	
 	
@@ -126,17 +233,5 @@ vector<int> largest_divisible_pairs(vector<int> digitsVector) {
 	FindLDPsubset(digitsVector, result, currSubset, sizeSubset, 0, 1);
 	
 	return result;
-}
+}*/
 
-
-int main() {
-	
-	vector<int> digitsVector{2, 3, 8, 14, 22, 24, 28, 44, 56};
-	//vector<int> digitsVector{22, 1, 44, 52, 6, 39, 14, 28, 88};
-	
-	vector<int> LDPvec = largest_divisible_pairs(digitsVector);
-	
-	cout << "Input: " << vec_to_string(digitsVector) << endl;
-	cout << "Answer: " << vec_to_string(LDPvec) << endl;
-	
-}
